ec8da5746b74bdd41a63acb7552a46c24620208b
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-11-05 18:15:09 +0700

    new version

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const API_KEY = "43437f96-2c13-4cae-af86-96ad63fbf88f";
-const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1";
+const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=3";
 
 getMovies(API_URL_POPULAR);
 
@@ -19,10 +19,7 @@ async function getMovies(url) {
             <div class="film__poster">
                 <img src="${movie.posterUrl}">
             </div>
-            <div class="film__hover"> ${movie.nameRu}</div>
-
         </div>
         `;
-        console.log(movie)
     });
 }
