623ffc574b6b765308813df9765295a8bb21db0b
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-11-19 17:07:27 +0700

    first full version

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const API_KEY = "43437f96-2c13-4cae-af86-96ad63fbf88f";
-const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=3";
+const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=2";
 
 getMovies(API_URL_POPULAR);
 
@@ -13,11 +13,26 @@ async function getMovies(url) {
     const respData = await resp.json();
 
     respData.films.forEach(movie => {
+        movie.rating = +movie.rating;
+        let type = "white";
+        if (movie.rating <= 7.7) {
+            type = "rgba(255, 0, 0, 0.5)";
+            console.log("fined")
+        } else if (movie.rating < 8) {
+            type = "rgba(255, 255, 0, 0.5)";
+        } else if (movie.rating >= 8) {
+            type = "rgba(0, 255, 0, 0.5)";
+        }
         document.querySelector('.films').innerHTML +=
             `
         <div class="film">
-            <div class="film__poster">
-                <img src="${movie.posterUrl}">
+            <img class="film__img" src="${movie.posterUrl}">
+            
+            <div class="film__body">
+                <div class="film__bottom">
+                    <div class="film__rating" style="background-color: ${type}">${movie.rating}</div>
+                    <div class="film__year">${movie.year}</div>
+                </div>
             </div>
         </div>
         `;
