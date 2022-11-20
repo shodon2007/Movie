48e4d6df7f554cfecac16aa6f6f54e457e1c7099
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-11-20 10:36:57 +0700

    added header burger

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const API_KEY = "43437f96-2c13-4cae-af86-96ad63fbf88f";
-const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=2";
+const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1";
 
 getMovies(API_URL_POPULAR);
 
