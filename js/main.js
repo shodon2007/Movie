3a50920af50bbf4f6854893d3fffd90c2f3f2701
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-12-02 17:19:41 +0700

    bags fixed and code refactored

--- js/main.js
+++ js/main.js
@@ -1,24 +1,5 @@
 const API_KEY = "43437f96-2c13-4cae-af86-96ad63fbf88f";
 const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1";
-let isBurger = false;
-const burger = document.querySelector('.burger');
-const burgerMenu = document.querySelector('.burger__menu');
-let burgerClick = () => {
-    isBurger = isBurger == false ? true : false;
-
-    if (isBurger == true) {
-        burger.style.zIndex = 3;
-        burger.style.opacity = 1;
-        burgerMenu.style.right = "0px";
-    } else {
-        burger.style.opacity = 0;
-        burgerMenu.style.right = "300px";
-        setTimeout(() => {
-            burger.style.zIndex = -3;
-        }, 500);
-    }
-}
-
 
 
 
@@ -27,48 +8,51 @@ getMovies(API_URL_POPULAR);
 async function getMovies(url) {
     const resp = await fetch(url, {
         headers: {
-            'Access-Control-Allow-Origin': 'http://127.0.0.1:3000',
-            'Access-Control-Allow-Methods': 'POST',
-            'Access-Control-Allow-Headers': 'Content-Type',
             "Content-Type": 'application/json',
             "X-API-KEY": API_KEY,
         },
     });
-    resp.headers.append('GET', 'POST', 'OPTIONS');
     const respData = await resp.json();
 
     respData.films.forEach(movie => {
-        movie.rating = +movie.rating;
-        let type = "white";
-        if (movie.rating <= 7.7) {
-            type = "rgba(255, 0, 0, 0.5)";
-        } else if (movie.rating < 8) {
-            type = "rgba(255, 255, 0, 0.5)";
-        } else if (movie.rating >= 8) {
-            type = "rgba(0, 255, 0, 0.5)";
-        }
-        document.querySelector('.films .body').innerHTML +=
-            `
+        let rating = showRating(+movie.rating);
+
+        renderMovies(movie, rating)
+    });
+}
+
+function showRating(rating) {
+    if (rating <= 7.7) {
+        return "rgba(255, 0, 0, 0.5)";
+    } else if (rating < 8) {
+        return "rgba(255, 255, 0, 0.5)";
+    } else if (rating >= 8) {
+        return "rgba(0, 255, 0, 0.5)";
+    }
+}
+
+function renderMovies(movie, rating) {
+    document.querySelector('.films .body').innerHTML +=
+        `
         <div class="film">
             <img class="film__img" src="${movie.posterUrl}">
             
             <div class="film__body">
                 <div class="film__bottom">
-                    <div class="film__rating" style="background-color: ${type}">${movie.rating}</div>
+                    <div class="film__rating" style="background-color: ${rating}">${movie.rating}</div>
                     <div class="film__year">${movie.year}</div>
                 </div>
             </div>
         </div>
         `;
-    });
 }
 
-
 window.addEventListener('scroll', function () {
-    if (window.pageYOffset >= 50) {
-        document.querySelector('header').style.position = 'fixed';
+    const header = document.querySelector('header');
+
+    if (window.pageYOffset >= header.offsetHeight) {
+        header.style.position = 'fixed';
     } else {
-        document.querySelector('header').style.position = 'relative';
-        document.querySelector('.films').style.marginTop = document.querySelector('header').offsetHeight;
+        header.style.position = 'relative';
     }
 });
